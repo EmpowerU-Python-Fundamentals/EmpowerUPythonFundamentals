5558c47fd129d4c94fd41cf19bf35fec7f013f0f
Volodymyr Yusyp <volodymyr.yusyp@gmail.com>
2025-07-22 21:44:42 +0300

    Переміщую файли в папку Astronaut-08

---
+++ hw/hw07/Astronaut-08/07.3 practical tasks by Astronaut-08.py
@@ -0,0 +1,87 @@
+'''Practical tasks by Astronaut-08'''
+
+#TASK 1 Jenny's secret message
+#Jenny has written a function that returns a greeting for a user.
+#However, she's in love with Johnny, and would like to greet him slightly different.
+#She added a special case to her function, but she made a mistake.
+
+
+#TASK 2 Find The Distance Between Two Points
+#Given two ordered pairs calculate the distance between them.
+#Round to two decimal places.
+#This should be easy to do in 0(1) timing.
+
+
+#TASK 3 No yelling!
+#Write a function taking in a string like WOW this is REALLY
+#amazing and returning Wow this is really amazing.
+#String should be capitalized and properly spaced.
+#Using re and string is not allowed.
+#Examples:
+#filter_words('HELLO CAN YOU HEAR ME') #=> Hello can you hear me
+#filter_words('now THIS is REALLY interesting') #=> Now this is really interesting
+#filter_words('THAT was EXTRAORDINARY!') #=> That was extraordinary!
+
+
+#TASK 4 Convert a Number to a String!
+#We need a function that can transform a number (integer) into a string.
+#What ways of achieving this do you know?
+#Examples (input --> output):
+#123  --> "123"
+#999  --> "999"
+#-100 --> "-100"
+
+
+#TASK 5 Reversing Words in a String
+#You need to write a function that reverses the words in a given string.
+#Words are always separated by a single space.
+#As the input may have trailing spaces, you will also need to ignore unneccesary whitespace.
+#Example (Input --> Output)
+#"Hello World" --> "World Hello"
+#"Hi There." --> "There. Hi"
+#Happy coding!
+
+
+#TASK 6 Reverse List Order
+#In this kata you will create a function that takes in a list and returns a list with the
+#reverse order.
+#Examples (Input -> Output)
+#* [1, 2, 3, 4]  -> [4, 3, 2, 1]
+#* [9, 2, 0, 7]  -> [7, 0, 2, 9]
+
+
+#TASK 7 Multiples of 3 or 5
+#If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6
+#and 9. The sum of these multiples is 23.
+#Finish the solution so that it returns the sum of all the multiples of 3 or 5 below
+#the number passed in.
+#Additionally, if the number is negative, return 0.
+#Note: If the number is a multiple of both 3 and 5, only count it once.
+
+
+#TASK 8 Will you make it?
+#You were camping with your friends far away from home, but when it's time to go back,
+#you realize that your fuel is running out and the nearest pump is 50 miles away!
+#You know that on average, your car runs on about 25 miles per gallon. There are 2 gallons
+#left.
+#Considering these factors, write a function that tells you if it is possible to get to
+#the pump or not.
+#Function should return true if it is possible and false if not.
+
+
+#TASK 9 Are You Playing Banjo?
+#Create a function which answers the question "Are you playing banjo?".
+#If your name starts with the letter "R" or lower case "r", you are playing banjo!
+#The function takes a name as its only argument, and returns one of the following strings:
+#name + " plays banjo" 
+#name + " does not play banjo"
+#Names given are always valid strings.
+
+
+#TASK 10 Convert boolean values to strings 'Yes' or 'No'.
+#Complete the method that takes a boolean value and return a "Yes" string for true, or a
+#"No" string for false.
+
+
+#TASK 11
+#TASK 12
