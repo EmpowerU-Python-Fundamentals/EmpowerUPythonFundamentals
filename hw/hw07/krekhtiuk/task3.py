ab477400b535a88525ff1cf0d57ea8f135474881
Vira Krekhtiuk <golkovira@gmail.com>
2025-07-26 00:45:01 +0300

    HW07: task3

---
+++ hw/hw07/krekhtiuk/task3.py
@@ -0,0 +1,8 @@
+def my_func(text):
+    result = {}
+    for letter in text:
+        if letter in result:
+            result[letter] += 1
+        else:
+            result[letter] = 1
+    return result
