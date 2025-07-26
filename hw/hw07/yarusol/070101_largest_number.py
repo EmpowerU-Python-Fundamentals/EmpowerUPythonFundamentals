b203beed6a753dd6e7d955cc28e7d28ed5058983
yarusol.smartphone <yarusol.smartphone@gmail.com>
2025-07-26 15:44:46 +0300

    Added the solutions for the HW 07.1

---
+++ hw/hw07/yarusol/070101_largest_number.py
@@ -0,0 +1,7 @@
+# Task1.
+# Write a function that returns the largest number of two numbers.
+# (Use DicStrings documentation strings in the function).
+
+def largest_of_two(first: int | float, second: int | float):
+    """Returns the largest number of two numbers"""
+    return max(first, second)
