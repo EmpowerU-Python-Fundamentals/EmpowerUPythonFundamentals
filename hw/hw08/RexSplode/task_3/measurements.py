6c756fcfd812aba7e32b5e47a458496e1684b0cf
RexSplode <zhaldak.ivan@gmail.com>
2025-07-27 16:45:35 +0300

    third task

---
+++ hw/hw08/RexSplode/task_3/measurements.py
@@ -0,0 +1,31 @@
+from math import pi, pow
+import re as regex
+
+def is_not_number(*inputs: float) -> bool:
+    for input in inputs:
+        number_pattern = regex.compile(r'^-?(?:\d+\.\d+|\d+)$')
+        if not number_pattern.match(input):
+            return True
+    return False
+
+def calc_triangle_area(base: float, height: float) -> float:
+    if is_not_number(base, height):
+        print("One of the params wasn't a number")
+        return 0.0
+    return round(0.5 * float(base) * float(height), 2)
+
+def calc_square_area(side1: float, side2: float) -> float:
+    if is_not_number(side1, side2):
+        print("One of the params wasn't a number")
+        return 0.0
+    return round(float(side1) * float(side2), 2)
+
+def calc_circle_area(radius: float) -> float:
+    if is_not_number(radius):
+        print("The parameter you entered is not a number")
+        return 0.0
+    return round(pow(float(radius), 2) * pi)
+
+
+
+
