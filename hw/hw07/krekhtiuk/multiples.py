2006168383948dcc4259bd0e417210c5ea0ea62b
Vira Krekhtiuk <golkovira@gmail.com>
2025-07-27 23:52:18 +0300

    HW07: multiples

---
+++ hw/hw07/krekhtiuk/multiples.py
@@ -0,0 +1,8 @@
+def solution(number):
+    if number < 0:
+        return 0
+    else:
+        return sum(x for x in range(number) if x%3 == 0 or x%5 == 0)
+print(solution(9))
+print(solution(-5))
+    
