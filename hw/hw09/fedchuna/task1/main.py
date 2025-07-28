4b9a2a662720d4e0040e7cfcce040e51a9024983
fedchuna <fedchuna@gmail.com>
2025-07-28 12:33:58 +0300

    HW_09_fedchuna_task1_added module

--- hw/hw09/fedchuna/task1/main.py
+++ hw/hw09/fedchuna/task1/main.py
@@ -1,5 +1,6 @@
-import pygame as pg 
+import pygame as pg
 import random
+import module as m
 pg.init()
 
 SCREEN_WIDTH = 800
@@ -23,51 +24,8 @@ clock = pg.time.Clock()
 Done = False
 
 
-class InputBox:
-    def __init__(self, x, y, w, h):
-        self.rect = pg.Rect(x, y, w, h)
-        self.color = INACTIVE_COLOR
-        self.text = text
-        self.active = False
-        self.font = font1
-
-    def draw(self, surface):
-        pg.draw.rect(surface, self.color, self.rect)
-        text_surface = self.font.render(self.text, True, BLACK)
-        surface.blit(text_surface, (self.rect.x + 5, self.rect.y + 5))
-        pg.draw.rect(surface, self.color, self.rect, 2)
-
-    def handle_event(self,event):
-        
-        if event.type == pg.MOUSEBUTTONDOWN:
-            self.text = ''
-            if self.rect.collidepoint(event.pos):
-                self.active = not self.active
-            else:
-                self.active = False
-            self.color = ACTIVE_COLOR if self.active else INACTIVE_COLOR
-        if event.type == pg.KEYDOWN:
-            if self.active:
-                if event.key == pg.K_RETURN:
-                    # f = self.text
-                    try:
-                        a = int(self.text)
-                        b = random.randint(1, 100)
-                        self.text = ''
-                        if a != b:
-                            self.text = f'You guessed {a}, but the number was {b}. Try again!'
-                        else:
-                            self.text = f'Congratulations! You guessed the number {b} correctly!'
-                    except ValueError:
-                        self.text = "Please enter a valid number"
-                        return self.text = ''
-                elif event.key == pg.K_ESCAPE:
-                    self.text = ''
-                elif event.key == pg.K_BACKSPACE:
-                    self.text = self.text[:-1]
-                elif event.unicode.isprintable():
-                    self.text += event.unicode
-input_box_i = input_box = InputBox(SCREEN_WIDTH // 2 - 300, SCREEN_HEIGHT // 2 - 16, 610, 32)
+# Create an instance of InputBox
+input_box_i = input_box = m.InputBox(SCREEN_WIDTH // 2 - 300, SCREEN_HEIGHT // 2 - 16, 610, 32)
 
 while not Done:
     for event in pg.event.get():
@@ -78,17 +36,14 @@ while not Done:
         elif event.type == pg.KEYDOWN:
             if event.key == pg.K_z:
                 Done = True
-            print(event)
-            a = input_box_i.handle_event(event)
-            if a is not None:
-                gameDisplay.blit(c, (200, 250))
-            else:
-                gameDisplay.blit(e, (200, 250))
+            input_box_i.handle_event(event)
 
     gameDisplay.fill(WHITE)
     input_box_i.draw(gameDisplay)
     
     pg.display.update()
     clock.tick(60)
-
-pg.quit()
+if __name__ == "__main__":
+    pg.quit()
+    exit()
+    
---
+++ hw/hw09/fedchuna/task1/module.py
@@ -0,0 +1,58 @@
+import pygame as pg
+import random
+pg.init()
+
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+ACTIVE_COLOR = (173, 215, 235)
+INACTIVE_COLOR = (200, 200, 200)  
+
+font1 = pg.font.SysFont('Calibri', 25, True, False)
+text = "input a number "
+
+
+class InputBox:
+    def __init__(self, x, y, w, h):
+        self.rect = pg.Rect(x, y, w, h)
+        self.color = INACTIVE_COLOR
+        self.text = text
+        self.active = False
+        self.font = font1
+
+    def draw(self, surface):
+        pg.draw.rect(surface, self.color, self.rect)
+        text_surface = self.font.render(self.text, True, BLACK)
+        surface.blit(text_surface, (self.rect.x + 5, self.rect.y + 5))
+        pg.draw.rect(surface, self.color, self.rect, 2)
+
+    def handle_event(self,event):
+        
+        if event.type == pg.MOUSEBUTTONDOWN:
+            self.text = ''
+            if self.rect.collidepoint(event.pos):
+                self.active = not self.active
+            else:
+                self.active = False
+            self.color = ACTIVE_COLOR if self.active else INACTIVE_COLOR
+        if event.type == pg.KEYDOWN:
+            if self.active:
+                if event.key == pg.K_RETURN:
+                    # f = self.text
+                    try:
+                        a = int(self.text)
+                        b = random.randint(1, 100)
+                        self.text = ''
+                        if a != b:
+                            self.text = f'You guessed {a}, but the number was {b}. Try again!'
+                        else:
+                            self.text = f'Congratulations! You guessed the number {b} correctly!'
+                    except ValueError:
+                        self.text = "Please enter a valid number"
+                        return None
+                elif event.key == pg.K_ESCAPE:
+                    self.text = ''
+                elif event.key == pg.K_BACKSPACE:
+                    self.text = self.text[:-1]
+                elif event.unicode.isprintable():
+                    self.text += event.unicode
