c087d2c8b3fe997c1f20402b3f51bcded8eeb4ab
yarusol.smartphone <yarusol.smartphone@gmail.com>
2025-07-28 22:54:58 +0300

    Completed the Task 08.1

---
+++ hw/hw08/yarusol/hw0801/modules/__init__.py
@@ -0,0 +1,5 @@
+__all__ = ["create_admin", "create_user"]
+
+from modules.admin import create_admin
+from modules.user import create_user
+
---
+++ hw/hw08/yarusol/hw0801/modules/user.py
@@ -0,0 +1,6 @@
+def create_user():
+    pass
+
+def some_user_function():
+    pass
+
