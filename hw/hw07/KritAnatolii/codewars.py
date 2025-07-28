523381e89b6f5ae60d7f97515e1736aa5f0e1dce
KritAnatolii <kritanatolii@gmail.com>
2025-07-28 20:50:14 +0300

    add 07.2 codewars

--- hw/hw07/KritAnatolii/codewars.py
+++ hw/hw07/KritAnatolii/codewars.py
@@ -32,3 +32,43 @@ def reverse_list(l):
     return l[::-1]
 
 #VII. Multiples of 3 or 5
+def solution(number):
+    if number <= 0:
+        return 0
+    return sum(i for i in range(number) if i % 3 == 0 or i % 5 == 0)
+
+#VIII. Will you make it?
+def zero_fuel(distance_to_pump, mpg, fuel_left):
+    return True if fuel_left * mpg >= distance_to_pump else False
+
+#IX. Are You Playing Banjo?
+def are_you_playing_banjo(name):
+    if name[0].lower() == "r":
+        answer = " plays banjo" 
+    else:
+        answer = " does not play banjo"
+    return name + answer
+
+#X. Convert boolean values to strings 'Yes' or 'No’
+def bool_to_word(boolean):
+    return "Yes" if boolean else "No"
+
+#XI. Counting sheep
+def count_sheeps(sheep):
+    count = 0
+    for sh in sheep:
+        if sh == True:
+            count +=1
+    return count
+
+#XI. Counting sheep
+def count_sheeps(sheep):
+    return sheep.count(True)
+
+#XII. Is this my tail?
+def correct_tail(body, tail):
+    sub = body[-1]
+    if sub == tail:
+        return True
+    else:
+        return False
