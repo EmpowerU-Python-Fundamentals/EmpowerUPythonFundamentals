8d4874e55c5d0f3232b9c06d23a0458639e08e92
Oleksandr_Urusov <urusov.oleksandr@gmail.com>
2025-07-28 17:51:30 +0300

    Add homework08

---
+++ hw/hw08/urrussov/task_1/models/admin.py
@@ -0,0 +1,8 @@
+__all__ = ['create_admin']
+
+def create_admin():
+    pass
+
+
+def check_admin_print():
+    print('This function just for check(admin)')
---
+++ hw/hw08/urrussov/task_1/utils/formatter.py
@@ -0,0 +1,9 @@
+__all__ = ['format_string']
+
+
+def format_string():
+    pass
+
+
+def check_format_print():
+    print('This function just for check(format)')
