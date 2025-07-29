c1508166c2fc290632181f6568d53cc0b014e3a9
Liubomyr Halamaha <galamagal86@gmail.com>
2025-07-29 15:46:23 +0300

    update path

--- hw/hw08/hw08_1/ivze0115/models/user.py
+++
@@ -1,7 +0,0 @@
-__all__ = ['create_user']
-
-def create_user():
-    pass
-
-def test_user():
-    pass
--- hw/hw08/hw08_1/ivze0115/utils/logger.py
+++
@@ -1,7 +0,0 @@
-__all__ = ['log_in_file']
-
-def log_in_file():
-    pass
-
-def test_log():
-    pass
