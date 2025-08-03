d9cf480abae967eeeed64277d6a777e46f9d0599
VadHk <vadim.huk@gmail.com>
2025-08-03 10:24:36 +0300

    task_2_4 HW10

--- hw/hw10/VadHk/task_2/task_2_4/main.py
+++ hw/hw10/VadHk/task_2/task_2_4/main.py
@@ -1,5 +1,9 @@
-def main():
-    pass
+from classes.person import Person
 
+def main():
+    john = Person("John", 30)
+    print(john.info)
+
+# https://www.codewars.com/kata/classy-classes
 if __name__ == "__main__":
     main()
