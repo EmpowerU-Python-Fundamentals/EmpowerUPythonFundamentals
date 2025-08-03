26e95275fce8790531715bee814f1a8720e8b6c0
Anastasiia Shkalikova <anastasia.shkalikova@gmail.com>
2025-08-03 20:45:11 +0300

    HW08: Add changes for 08.3 task (deboramay)

--- hw/hw08/deboramay/08.3/calculations.py
+++ hw/hw08/deboramay/08.3/calculations.py
@@ -1,14 +1,11 @@
 import math
 
-def area_rect(a, b):
-    S = a * b
-    return print(f"Площа прямокутника: {S}")
+def calculate_rectangle_area(a, b):
+    return f"Площа прямокутника: {a * b}"
 
-def area_tri(a, h):
-    S = 0.5 * a * h
-    return print(f"Площа трикутнкика: {S}")
+def calculate_triangle_area(a, h):
+    return f"Площа трикутник: {0.5 * a * h}"
 
-def area_circle(r):
-    S = math.pi * pow(r, 2)
-    return print(f"Площа круга: {S}")
+def calculate_circle_area(r):
+    return f"Площа кола: {math.pi * pow(r, 2)}"
 
--- hw/hw08/deboramay/08.3/main.py
+++ hw/hw08/deboramay/08.3/main.py
@@ -1,19 +1,24 @@
 import calculations
 
-figure = int(input("Площу якої фігури ви бажаєте розрахувати: 1 - прямокутник, 2 - трикутник," \
-"3 - коло. Введіть номер фігури: "))
+try:
+    figure = int(input("Площу якої фігури ви бажаєте розрахувати: 1 - прямокутник, 2 - трикутник, 3 - коло. Введіть номер фігури: "))
+except ValueError:
+    print(f"Ви ввели недопустиме значення. Введіть, будь ласка, цифру 1, 2 або 3, в залежності від вида фігури (де 1 - прямокутник, 2 - трикутник, " \
+"3 - коло). ")
+    exit()
 
 match figure:
     case 1:
         a = int(input("Введіть сторону а: "))
         b = int(input("Введіть сторону b: "))
-        calculations.area_rect(a, b)
+        print(calculations.calculate_rectangle_area(a, b))
     case 2:
         a = int(input("Введіть сторону а: "))
         h = int(input("Введіть висоту h: "))
-        calculations.area_tri(a, h)
+        print(calculations.calculate_triangle_area(a, h))
     case 3:
         r = int(input("Введіть радіус r: "))
-        calculations.area_circle(r)
+        print(calculations.calculate_circle_area(r))
     case _:
-        print("Помилка вводу, спробуйте ще раз.")
+        print(f"Ви ввели недопустиме значення: {figure}. Введіть, будь ласка, цифру 1, 2 або 3, в залежності від вида фігури (де 1 - прямокутник, 2 - трикутник, " \
+"3 - коло) ")
