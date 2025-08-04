b5bf2d74d8865fb1ac5086d8451f81f879884374
Vasia Zozulia <Vasia.Zozulia@live.com>
2025-08-04 11:29:45 +0300

    Fixed variables names

--- hw/hw09/VasiaZozulia/Task1.py
+++ hw/hw09/VasiaZozulia/Task1.py
@@ -1,59 +1,56 @@
 import random
 
 # Define the range within which the number will be guessed
-guess_range = (1, 100)
+MIN_NUMBER = 1
+MAX_NUMBER = 100
 
 # Set the maximum number of attempts the user has to guess the number
-tries = 10
+ATTEMPTS = 10
 
-# Dictionary containing hints for the user's guesses
-tips = {
-    'small': 'Your number is too small',
-    'big': 'Your number is too hight',
-    'win' : 'Congratulations, you won the game!',
-    'loss' : 'Unfortunately, you lost the game. All your attempts were unsuccessful.',
-    'input_guess_message' : 'Enter your assumption: ',
-    'range_error' : 'The range within which the number will be guessed is incorrect. The first value should be smaller than the second value.',
-    'input_number_error' : 'Please enter the whole number!',
-}
+# Hints for the user's guesses
+SMALL = 'Your number is too small'
+BIG = 'Your number is too hight'
+WIN = 'Congratulations, you won the game!'
+LOSS = 'Unfortunately, you lost the game. All your attempts were unsuccessful.'
+INPUT_GUESS_MESSAGE = 'Enter your assumption: '
+RANGE_ERROR = 'The range within which the number will be guessed is incorrect. The first value should be smaller than the second value.'
+INPUT_NUMBER_ERROR = 'Please enter the whole number!'
 
 # Checking if the guess range is within the correct bounds.
 try:
     # Randomly select a number between the specified range
-    random_number = random.randint(guess_range[0], guess_range[1])
+    random_number = random.randint(MIN_NUMBER, MAX_NUMBER)
 
     print ("Welcome to the game 'Guess the Number.'")
-    print (f"I’ve picked a number between {guess_range[0]} and {guess_range[1]}. You have {tries} tries to guess it. Let's play!")
+    print (f"I’ve picked a number between {MIN_NUMBER} and {MAX_NUMBER}. You have {ATTEMPTS} tries to guess it. Let's play!")
 
     # Start the game
-    while tries > 0:
+    while ATTEMPTS > 0:
         try:
-            guess = int(input(tips['input_guess_message']))
+            guess = int(input(INPUT_GUESS_MESSAGE))
         except ValueError:
-            print (tips['input_number_error'])
+            print (INPUT_NUMBER_ERROR)
             continue
 
         # Checking if the user's number is within the correct bounds.
-        if guess < guess_range[0] or guess > guess_range[1]:
-            print (f"The number should be between {guess_range[0]} and {guess_range[1]}")
+        if guess < MIN_NUMBER or guess > MAX_NUMBER:
+            print (f"The number should be between {MIN_NUMBER} and {MAX_NUMBER}")
             continue
 
         # Checking if the user's input number equal random number
         if guess == random_number:
-            print (f"{tips['win']}. The number was {random_number}.")
-            game_result = True
+            print (f"{WIN}. The number was {random_number}.")
             break
         elif guess < random_number:
-            print (f"{tips['small']}.")
+            print (f"{SMALL}.")
         else:
-            print (f"{tips['big']}.")
+            print (f"{BIG}.")
 
         # User attempts decrease each time they try to guess the number.
-        tries -= 1
-        print (f"{tries} tries left! Try again!")
+        ATTEMPTS -= 1
+        print (f"{ATTEMPTS} tries left! Try again!")
     else:
-        print (f"{tips['loss']} The number was {random_number}.")
-
+        print (f"{LOSS} The number was {random_number}.")
 
 except ValueError:
-    print (tips['range_error'])
+    print (RANGE_ERROR)
