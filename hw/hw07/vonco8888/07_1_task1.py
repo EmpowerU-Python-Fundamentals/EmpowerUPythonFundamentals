09b396d00300bdea685da99efbb968074600fd84
vonco8888 <95396260+vonco8888@users.noreply.github.com>
2025-08-05 00:58:34 +0300

    Create 07_1_task1.py

---
+++ hw/hw07/vonco8888/07_1_task1.py
@@ -0,0 +1,15 @@
+def get_larger_number(a, b):
+    """
+    Повертає більше з двох переданих чисел.
+
+    Аргументи:
+    a (int або float): перше число
+    b (int або float): друге число
+
+    Повертає:
+    int або float: більше з двох чисел
+    """
+    if a > b:
+        return a
+    else:
+        return b
