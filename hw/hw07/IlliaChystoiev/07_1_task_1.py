e8edd3a9e4cde99caa45875c90068eff770c6a6f
IlliaChystoiev <4i100ev@gmail.com>
2025-08-04 15:45:31 +0300

    hw07: Practical Task 07.1 - IlliaChystoiev

---
+++ hw/hw07/IlliaChystoiev/07_1_task_1.py
@@ -0,0 +1,7 @@
+# Task1. Write a function that returns the largest number of two numbers (use DocStrings documentation strings in the function).
+
+def largest_number(a, b):
+    """
+    Returns the largest of two numbers.
+    """
+    return a if a > b else b
