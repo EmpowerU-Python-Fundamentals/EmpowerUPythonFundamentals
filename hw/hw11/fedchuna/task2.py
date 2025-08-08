bbaf41cbac17587566bae945a0b78d2c0fe4a93f
fedchuna <fedchuna@gmail.com>
2025-08-08 15:35:57 +0300

    Task1 and 2 done

--- hw/hw11/fedchuna/task2.py
+++ hw/hw11/fedchuna/task2.py
@@ -1,18 +1,28 @@
-def check_age(age):
-    if age < 0:
-        raise ValueError("Your age cannot be negative.")
-    if age % 2 == 0:
-        return("Your age is an even .")
-    else:
-        return("Your age is odd.")
+### 2. Напишите программу, которая анализирует введенное число и, в зависимости от числа, выдает день недели, соответствующий этому числу (1 - понедельник, 2 - вторник и т. д.). Учтите случаи ввода чисел от 8 и более, а также случаи ввода нечисловых данных.
 
+def day_of_week(day_number):
+    match day_number:
+        case 1:
+            return "monday"
+        case 2:
+            return "tuesday"
+        case 3:
+            return "wednesday"
+        case 4:
+            return "thursday"
+        case 5:
+            return "friday"
+        case 6:
+            return "saturday"
+        case 7:
+            return "sunday"
+        case _:
+            raise ValueError("Number must be between 1 and 7.")
+        
 while True:
     try:
-        user_age_str = input("Please input your number: ")
-        user_age = int(user_age_str)
-        print(check_age(user_age))
+        user_input = int(input("Enter numerical number of the day of the week (1-7): "))
+        print(f"Today is {day_of_week(user_input)}")
         break
     except ValueError as e:
-        print(f"Error: {e}")
-    except TypeError:
-        print("Error: Input data is not a number.")
+        print(f"Error inpt data is not a number: {e}")
