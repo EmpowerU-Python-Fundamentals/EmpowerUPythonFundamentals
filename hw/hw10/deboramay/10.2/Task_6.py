362b208d446ed0b78f7356e6469bb84a1daacb3b
Anastasiia Shkalikova <anastasia.shkalikova@gmail.com>
2025-08-12 12:05:35 +0300

    HW10: add hw10 by deboramay

---
+++ hw/hw10/deboramay/10.2/Task_6.py
@@ -0,0 +1,19 @@
+#Python's Dynamic Classes
+
+def class_name_changer(cls, new_name):
+    if not new_name[0].isupper() or not new_name.isalnum():
+        raise Exception("Invalid class name")
+    cls.__name__ = new_name
+
+#Test:
+class MyClass:
+    pass
+
+myObject = MyClass()
+print(MyClass.__name__, "MyClass")
+
+class_name_changer(MyClass, "UsefulClass")
+print(MyClass.__name__, "UsefulClass")
+
+class_name_changer(MyClass, "SecondUsefulClass")
+print(MyClass.__name__, "SecondUsefulClass")
