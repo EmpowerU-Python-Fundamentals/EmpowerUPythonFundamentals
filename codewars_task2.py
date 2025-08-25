74d84c8866949fec58b295dcd109e32fe7b7b134
Liubomyr Halamaha <galamagal86@gmail.com>
2025-08-25 14:27:23 +0300

    fix

--- codewars_task2.py
+++
@@ -1,9 +0,0 @@
-#II. Find The Distance Between Two Points
-
-import math
-
-def distance(x1, y1, x2, y2):
-    dist = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
-    return round(dist, 2)
-print(distance(0, 0, 3, 4)) 
-print(distance(1, 2, 4, 6))
