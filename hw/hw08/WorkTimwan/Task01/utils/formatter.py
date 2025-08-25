d4a056de94f66c1c79d07ff6449a9affe91527d2
Timwan <mworkwithit2005@gmail.com>
2025-08-25 22:57:46 +0300

    HW06: Add Learn Git Branching screenshots for WorkTimwan

---
+++ hw/hw08/WorkTimwan/Task01/utils/formatter.py
@@ -0,0 +1,12 @@
+#from models.users import *
+#from models.admin import *
+#from utils.logger import *
+
+
+#__all__ = ["format_string"]
+
+
+def format_string ():
+    pass
+
+__all__ = ["format_string"]
