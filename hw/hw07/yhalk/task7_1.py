34b26032fa9187cce2faa83679bb792e86f9c404
Yurii Halkevych <yuriihalkevych.com>
2025-08-28 22:06:33 +0300

    in progress

--- hw/hw07/yhalk/task7_1.py
+++ hw/hw07/yhalk/task7_1.py
@@ -5,5 +5,17 @@ def compare(first, second):
     else:
         print(second)
 
+def rectangle_area(a,b):
+    """this function returns an area of a rectangle"""
+    return a*b
 
+def triangle_area(a,h):
+    """this function returns an area of a triangle, where a is base and h is a height"""
+    return (a*h)/2
+
+def circle_area(r):
+    """this function returns an area of a circle, where r is a radius"""
+    return 3.14*r*r
+
+print(circle_area(3))
 
