5b62995c35d4ced5a60d311b63f150c2fb011239
bherych <bogdangerych@gmail.com>
2025-08-31 09:50:44 +0300

    HW09: finished 09.1 task for bherych

---
+++ hw/hw09/bherych/task9_1.py
@@ -0,0 +1,29 @@
+import random
+
+def check_number(guess, number):
+    if guess > number:
+        print("Bigger")
+    elif guess < number:
+        print("Smaller")
+    elif guess == number:
+        print("You win")
+        return True
+    return False
+
+def game():
+    print("Try to guess a number between 1 and 100. You have 10 attempts.")
+    max_attempts = 10
+    attempts = 0
+    number = random.randint(1, 100)
+
+    while attempts <= max_attempts:
+        try:
+            guess = int(input("Enter your guess: "))
+        except ValueError:
+            print("Invalid input, try to enter the whole number")
+            continue
+        attempts += 1
+        if check_number(guess, number):
+            break
+
+game()
